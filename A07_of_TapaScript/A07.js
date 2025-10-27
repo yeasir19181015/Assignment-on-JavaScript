8d855aedd2bcf285642abb04f347a350a29707f2
yeasir1918015 <yeasirarafat688@gmail.com>
2025-10-27 18:28:30 +0600

    assignment 7 done

--- A07_of_TapaScript/A07.js
+++ A07_of_TapaScript/A07.js
@@ -2,11 +2,86 @@
 // Change the if-else to switch-case as asked in the video(at 15:42).
 
 
+/**
+ * The Rock 🪨, Paper 🧻, or Scissors ✂️ Game
+ * This game will be played between the computer and human. Once Human will select Rock, Paper, or Scissors and in the next turn the computer will select Rock, Paper, or Scissors.
+ * The Winner will be selected based on these rules:
+ * - Rock can Break Scissors => Rock Wins
+ * - Paper can wrap Rock => paper Wins
+ * - Scissors can cut Paper => Scissors Wins
+ * Ask the input from user and randomly select a computer selection to continue the game.
+ */
+
+// Assumptions:
+// 1. We have to prompt the user to get their inputs.
+// 2. The Computer's selection will be random.
+// 3. We have to compare user and computer's choice
+// 4. We need to announce the winner.
+// 5. After the Winner announcement, we may want to ask the user to play again or quit from the game.
+
+function rockPaperScissorsGame() 
+{
+    console.log("Getting Started With the Rock 🪨, Paper 🧻, or Scissors ✂️ Game");
+    const userChoicePrompt = prompt("Enter Rock 🪨, Paper 🧻, or Scissors ✂️");
+    const userChoice = userChoicePrompt.toLowerCase();
+
+    let computerChoice;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+        computerChoice = "rock";
+    } else if (randomNumber === 2) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    switch(randomNumber){
+        case 1 : computerChoice = "rock";
+        break;
+        case 2 : computerChoice = "paper";
+        break;
+        default : computerChoice = "scissors"
+        break;
+    }
+   
+
+    console.log("User selected", userChoice);
+    console.log("Computer selected", computerChoice);
+
+    if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("You the User WIN, yay!!!!");
+    } else if ( userChoice === computerChoice) {
+        console.log("The Game is a Tie");
+    } else if(
+        (userChoice === "rock" && computerChoice === "paper") ||
+        (userChoice === "paper" && computerChoice === "scissors") ||
+        (userChoice === "scissors" && computerChoice === "rock")
+    ) {
+        console.log("Oh Ho... Computer Wins!!!!");
+    } else {
+        console.log("Please check the input, We didn't understand it.")
+    }
+
+
+    const playAgainPrompt = prompt("Do You Want to Play Again? (yes/no)")
+    const playAgain = playAgainPrompt ? playAgainPrompt.toLowerCase() : "no";
+
+    if (playAgain === "yes") {
+        rockPaperScissorsGame();
+    } else {
+        console.log("Thanks for Playing! See you Next Time")
+    }
+}
+
+// Start This Game
+rockPaperScissorsGame();
+
+
 
 
 // 2. Build the Secret Number Guessing Game
-// Create the project as explained in the video.
-
-function rockPaperScissorsGame() {
-
-}
+// Create the project as explained in the video. 
